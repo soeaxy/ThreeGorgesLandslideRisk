e3f49b535ebdd0dfa020a21885a162c0c2f90e19
soeasy <869546849@qq.com>
2020-04-18 10:58:32 +0800

    上传模型

--- landslideRisk.py
+++ landslideRisk.py
@@ -1,1 +1,19 @@
-print('Three Gorges')
+import pandas as pd 
+
+data = pd.read_csv('landslideSample.txt')
+print(data.head())
+
+data.dropna(axis=0, how='any', inplace=True)
+print(data.head())
+print(data.tail())
+
+xCol = [item for item in data.columns if item not in ['OBJECTID','lon','lat','Class']]
+X = data[xCol]
+y = data['Class']
+GeoID = data['OBJECTID']
+print(data['Class'].value_counts())
+
+
+
+
+
